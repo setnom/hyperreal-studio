379ef99744751ff6f94910180e6886c8f0f7f7dd
Juan Esteban Montes Henao <juanmontesh@MacBook-Pro.local>
2026-04-14 15:11:15 +1000

    v36l: Cambiar plan botón verde neón fluorescente

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
+import App, { ErrorBoundary } from './App.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+)
